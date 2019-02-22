90e694b2376cc014f08a3fc7edbd7bfb31dd8b02
kajames2 <james144@mail.chapman.edu>
2019-02-21 18:16:43 -0800

    modified life enjoyment penalties.  Added output readers

---
+++ output/generated_life_reader.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+
+filenames = ["actuarial_flat_45_100",
+             "actuarial_flat_45_95",
+             "actuarial_flat_45_90",
+             "actuarial_flat_60_100",
+             "actuarial_flat_60_95",
+             "actuarial_flat_60_90",
+             "actuarial_flat_75_100",
+             "actuarial_flat_75_95",
+             "actuarial_flat_75_90",
+             "actuarial_increasing_30_90_100",
+             "actuarial_increasing_30_90_95",
+             "actuarial_increasing_30_90_90"
+]
+dfs = {filebase : pd.read_csv(filebase + "_rand_lives.csv") for filebase in filenames}
+ages = [1] + list(range(5,61,5))
+
+for col in ['Age', 'Cash', 'Shocks', 'ShockProb', 'Fitness', 'InsurancePayout', 'BuyIns', 'Enjoyment', 'TotalJoy', 'JoySpending', 'FitnessSpending', 'InsuranceSpending', 'NextCash']:
+    print()
+    print(col)
+    for filebase in filenames:
+        means = [(dfs[filebase].loc[dfs[filebase]['Age'] == age])[col].mean() for age in ages]
+        print("{0:35}".format(filebase) + "  " + ' '.join(["{0:7.2f}".format(mean) for mean in means]) + " --- " + "{0:7.2f}".format(np.mean(means)))
+
+        
+
+#def get_death_period(life):
+#    dead_pers = [int(row[0]) for row in life if float(row[header_indices["EndHealth"]]) <= 0]
+#    if dead_pers:
+#        return dead_pers[0]
+#    else:
+#        return max_age + 1
+
+#death_periods = [get_death_period(life) for life in rand_lives]
+
+#death_arr = [0]*(max_age + 1)
+#for per in death_periods:
+#    death_arr[per-1] += 1
+
+#print(death_arr)
--- src/healthcare/enjoyment/fractional.cc
+++ src/healthcare/enjoyment/fractional.cc
@@ -1,17 +1,18 @@
 #include "healthcare/enjoyment/fractional.h"
 
 #include <algorithm>
+#include <boost/math/constants/constants.hpp>
 
 namespace healthcare {
 namespace enjoyment {
-
 Fractional::Fractional(double j, int max_shocks, double shock_emph)
     : j_(j), max_shocks_(max_shocks), shock_emph_(shock_emph) {}
 
 double Fractional::GetEnjoyment(int shocks, int life_investment) const {
+  static const double pi = boost::math::constants::pi<double>();
   double base_enjoyment = life_investment / (life_investment + j_);
-  double shock_penalty = shock_emph_ * shocks / static_cast<double>(max_shocks_);
-  return (1 - shock_penalty) * base_enjoyment;
+  double shock_penalty = shock_emph_ * std::sqrt(std::cos(pi/(2 * max_shocks_)*shocks));
+  return shock_penalty * base_enjoyment;
 }
 
 }  // namespace enjoyment
