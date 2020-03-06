60599f772cc3878f3a807131359eeb24aebb9a64
Kevin James <james144@mail.chapman.edu>
2020-03-06 10:01:03 -0800

    fixed python files and modifiers

---
+++ include/healthcare/modifier/sqrt_cosine.h
@@ -0,0 +1,30 @@
+#ifndef HEALTHCARE_MODIFIER_SQRT_COSINE_H_
+#define HEALTHCARE_MODIFIER_SQRT_COSINE_H_
+
+#include <cmath>
+
+#include <boost/math/constants/constants.hpp>
+#include "healthcare/modifier.h"
+
+namespace healthcare {
+namespace modifier {
+
+class SqrtCosine : public Modifier {
+ public:
+  SqrtCosine(Param param, Func func, float max_modification, int max_param)
+      : Modifier(param, func),
+        max_param_(max_param),
+        max_modification_(max_modification) {}
+  float GetModification(int param) const override {
+    static const double pi = boost::math::constants::pi<double>();
+    return (1 - (max_modification_ * (1 - std::sqrt(std::cos(pi / (2 * max_param_) * param)))));
+  }
+
+  int max_param_;
+  float max_modification_;
+};
+
+}  // namespace modifier
+}  // namespace healthcare
+
+#endif  // HEALTHCARE_MODIFIER_SQRT_COSINE_H_
